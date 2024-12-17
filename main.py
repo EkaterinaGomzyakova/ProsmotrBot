6d174e159923bd12b67089b0535f350a8c369d12
EkaterinaGomzyakova <kate.gm717@gmail.com>
2024-12-18 02:16:57 +0300

    change platform to render

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
-from aiogram.enums.parse_mode import ParseMode
+from aiogram.types import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 import config
